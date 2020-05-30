6625573ac3e00ea38b9264feee868a0e86b456dd
Vincent Nguyen <vpvnguyen@gmail.com>
2020-05-29 21:17:09 -0700

    call fetchWeather and display weather card

---
+++ client/src/App.js
@@ -0,0 +1,63 @@
+import React, { useState } from "react";
+
+import { fetchWeather } from "./api/fetchWeather.js";
+import "./App.css";
+
+const App = () => {
+  const [query, setQuery] = useState("");
+  const [weather, setWeather] = useState({});
+
+  const search = async (e) => {
+    // if Enter is pressed, make api call
+    if (e.key === "Enter") {
+      const data = await fetchWeather(query);
+
+      console.log(data);
+      setWeather(data);
+
+      // reset weather
+      setQuery("");
+    }
+  };
+
+  return (
+    <div className="main-container">
+      <input
+        type="text"
+        className="search"
+        placeholder="Search..."
+        value={query}
+        onChange={(e) => setQuery(e.target.value)}
+        onKeyPress={search}
+      />
+      {/* if weather.main exists, render component */}
+      {weather.main && (
+        <div className="city">
+          <h2 className="city-name">
+            <span>{weather.name}</span>
+            {/* superscript element; works like exponent format */}
+            <sup>{weather.sys.country}</sup>
+          </h2>
+          {/* round the temp */}
+          <div className="city-temp">
+            {Math.round(weather.main.temp)}
+            {/* degree sign */}
+            <sup>&deg; C</sup>
+          </div>
+          <div className="info">
+            {/* icon of weather */}
+            <img
+              className="city-icon"
+              src={`https://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`}
+              alt={weather.weather[0].description}
+            />
+            {/* description of weather */}
+            <p>{weather.weather[0].description}</p>
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default App;
